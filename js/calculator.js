8b2a8349e9546421059e00da54f0be64b19c1953
sawsoni <soni4naresh@gmail.com>
2018-06-20 09:37:54 +0530

    adding space

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,11 @@
-function add(n1,n2){
+function add( n1, n2 ){
 	return n1 + n2;
 }
 
-console.log(add(10,20));
+console.log( add(10, 20) );
+
+function sub( n1, n2 ){
+	return n1 - n2;
+}
+
+console.log( sub(10, 20) );
