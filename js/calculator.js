e08174be1445f02978d2b549c5bc72c3dfecd9ce
sawsoni <soni4naresh@gmail.com>
2018-06-20 10:11:17 +0530

    changed in calculator module

--- js/calculator.js
+++ js/calculator.js
@@ -1,18 +1,28 @@
-function add( n1, n2 ){
+var Calculator = (function(){
+
+	function add( n1, n2 ){
 	return n1 + n2;
-}
+	}
 
-console.log( add(10, 20) );
 
-function sub( n1, n2 ){
+
+	function sub( n1, n2 ){
 	return n1 - n2;
-}
+	}
 
-console.log( sub(10, 20) );
 
-function multiply( n1, n2){
+
+	function multiply( n1, n2){
 	return n1 * n2;
-}
+	}
 
-multiply( 10, 20);
+	return {
+		add,
+		sub,
+		multiply
+	}
 
+
+})();
+
+	console.log(Calculator);
