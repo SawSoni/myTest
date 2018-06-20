b508d7e37932f8009247d9421a604e791d2a9032
sawsoni <soni4naresh@gmail.com>
2018-06-20 11:43:40 +0530

    many change in all file

--- js/calculator.js
+++ js/calculator.js
@@ -1,7 +1,7 @@
 var Calculator = (function(){
 
 	function add( n1, n2 ){
-	return n1 + n2;
+	return parseInt(n1) + parseInt(n2);
 	}
 
 
@@ -26,3 +26,11 @@ var Calculator = (function(){
 })();
 
 	console.log(Calculator);
+
+	addBtn.addEventListener('click',function(){
+		var n1 = document.getElementById('num1').value;
+		var n2 = document.getElementById('num2').value;
+
+		var result = document.getElementById('result');
+		result.innerHTML =  Calculator.add(n1,n2);
+	})
